e45edff0ff166207ed7c0e5cec1d1f6070e06227
Kathy Dieser <katdiese@gmail.com>
2016-04-06 13:37:55 -0600

    included loading gif

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,19 +1,19 @@
 app.controller('pokemonController', ['$scope', '$http', function($scope, $http) {
-  $scope.greeting = "I am the pokemon controller! :)";
-  $scope.randPokemon = function() {
-    return Math.floor((Math.random() * 739) + 1);
-  }
-  $scope.randAbility = function() {
-    return Math.floor((Math.random() * 400) + 1);
-  }
   $scope.newPokemon = function() {
-    $http.get('http://pokeapi.co/api/v2/pokemon/' + $scope.randPokemon()).then(function(data) {
+    $scope.pokeName = "";
+    $scope.pokeImage = "";
+    $scope.loading = true;
+    var random = Math.floor((Math.random() * 739) + 1);
+    $http.get('http://pokeapi.co/api/v2/pokemon/' + random).then(function(data) {
     $scope.pokeName = data.data.name;
     $scope.pokeImage = data.data.sprites.front_default;
 })
   }
   $scope.newAbility = function() {
-    $http.get('http://pokeapi.co/api/v2/ability/' + $scope.randAbility()).then(function(data) {
+    $scope.abilityLoad = true;
+    $scope.ability = "";
+    var random = Math.floor((Math.random() * 639) + 1);
+    $http.get('http://pokeapi.co/api/v2/move/' + random).then(function(data) {
       $scope.ability = data.data.name;
     })
   }
